77f85dff07a1df9cb3ab5557cf99e09d34a95e6f
Leo Torres <leojts24@gmail.com>
2022-04-27 21:49:44 -0600

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd7f1527e9f4ae8ba710102d76bed7929'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'db9facc6-c0cb-4078-b855-27c6a6e7f4a1'
+};
